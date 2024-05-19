d85ea73bf9f5fdb276868ca30b033de73ace56bd
UrielUriel <paredesuri@gmail.com>
2024-05-18 21:53:06 -0300

    [18/05/2024] Incluídos disclaimers de cláusulas de exención de responsabilidad

--- buscador.php
+++ buscador.php
@@ -30,6 +30,8 @@ if(!empty($_POST["buscar"])) {
                 </form>
             </div>
         </nav>
+        <h1 style="text-align:center;">Bienvenido al Buscador</h1>
+        <h4 style="text-align:center;">Ingrese texto en la esquina superior derecha para realizar una búsqueda en las siguientes bases de datos:</h2>
         <nav>
             <div class="nav nav-tabs" id="nav-tab" role="tablist">
                 <button class="nav-link active" id="nav-acm-tab" data-bs-toggle="tab" data-bs-target="#nav-acm" type="button" role="tab" aria-controls="nav-acm" aria-selected="true">ACM</button>
@@ -40,69 +42,93 @@ if(!empty($_POST["buscar"])) {
         </nav>
         <div class="tab-content" id="nav-tabContent">
             <div class="tab-pane fade show active" id="nav-acm" role="tabpanel" aria-labelledby="nav-acm-tab">
+                <p>La Biblioteca Digital ACM es una plataforma de investigación, descubrimiento y networking que contiene:</p>
+                <ul>
+                    <li>La colección de texto completo de todas las publicaciones de ACM, incluyendo revistas, actas de congresos, revistas técnicas, boletines y libros.</li>
+                    <li>Una colección de publicaciones a texto completo de editoriales seleccionadas.</li>
+                    <li>"The ACM Guide to Computing Literature", una completa base de datos bibliográfica centrada exclusivamente en el campo de la informática.</li>
+                    <li>Un conjunto de conexiones entre autores, obras, instituciones y comunidades especializadas.</li>
+                    <li>La <a href="https://dl.acm.org/about/dlboard">Junta de la Biblioteca Digital ACM</a>, que supervisa el diseño, las operaciones y la dirección de la Biblioteca Digital ACM y las plataformas tecnológicas de apoyo.</li>
+                </ul>
                 <?php
-                    $doc = new DOMDocument();
-                    libxml_use_internal_errors(true);
                     if(!empty($_POST["buscar"])) {
                         $url=$ParserACM->getUrlBusqueda();
-                        //$url="https://dl.acm.org/action/doSearch?AllField=".str_replace(' ', '+', $_POST["buscar"]);
                     }
                     else {
                         $url="https://dl.acm.org";
                     }
-                    /*$doc->loadHTMLFile($url);
-                    echo $doc->saveHTML();*/
                 ?>
-                <a <?php echo "href='".$url."'"; ?> target="_blank">Enlace a ACM</a>
+                <a <?php echo "href='".$url."'"; ?> target="_blank"><button type="button" class="btn btn-primary">Enlace a ACM</button></a>
             </div>
             <div class="tab-pane fade" id="nav-sciencedirect" role="tabpanel" aria-labelledby="nav-sciencedirect-tab">
+                <p>ScienceDirect es una base de datos en el mundo de la literatura científica, técnica y de salud revisada por pares y a texto completo. Sus funciones facilitan mantenerse informado y trabajar de manera más eficiente.</p>
+                <p>Incluye más de 2.900 revistas revisadas por pares.</p>
+                <ul>
+                    <li>21 millones de artículos y capítulos de libros</li>
+                    <li>800 revistas de acceso libre</li>
+                    <li>3,3 millones de artículos de acceso libre</li>
+                </ul>
+                <p>Además, incluye una cobertura completa de todas las disciplinas con las editoriales Academic Press, Woodhead Publishing y W.B. Saunders. Se puede buscar, leer y trabajar libremente en libros y revistas.</p>
+                <ul>
+                    <li>46.000 libros</li>
+                    <li>1.000.000 de autores</li>
+                    <li>Más de 50 editoriales</li>
+                </ul>
+                <p>Ofrece una comprensión más profunda de más de 363.000 temas clave, términos desconocidos y conceptos con explicaciones integradas directamente en la experiencia de lectura. </p>
                 <?php
-                    $doc = new DOMDocument();
-                    libxml_use_internal_errors(true);
                     if(!empty($_POST["buscar"])) {
                         $url=$ParserScienceDirect->getUrlBusqueda();
-                        //$url="https://www.sciencedirect.com/search?qs=".str_replace(' ', '%20', $_POST["buscar"]);
                     }
                     else {
                         $url="https://www.sciencedirect.com";
                     }
-                    /*$doc->loadHTMLFile($url);
-                    echo $doc->saveHTML();*/
                 ?>
-                <a <?php echo "href='".$url."'"; ?> target="_blank">Enlace a ScienceDirect</a>
+                <a <?php echo "href='".$url."'"; ?> target="_blank"><button type="button" class="btn btn-primary">Enlace a ScienceDirect</button></a>
             </div>
             <div class="tab-pane fade" id="nav-ieee" role="tabpanel" aria-labelledby="nav-ieee-tab">
+                <p>IEEE Xplore es una plataforma digital para el descubrimiento y el acceso a contenidos científicos y técnicos publicados por el IEEE (Institute of Electrical and Electronic Engineers) y sus socios editoriales.</p>
+                <p>IEEE Xplore contiene más de 6 millones de documentos y otros materiales de algunas de las publicaciones más citadas del mundo en ingeniería eléctrica, ciencias de la computación y otras ciencias relacionadas.</p>
+                <p>El contenido de IEEE Xplore comprende:</p>
+                <ul>
+                    <li>Más de 1,5 millones de artículos de investigación</li>
+                    <li>Más de 4 millones de artículos de conferencias</li>
+                    <li>Más de 14.000 normas técnicas</li>
+                    <li>Más de 66.000 libros y capítulos de libros</li>
+                    <li>Más de 500 cursos educativos en línea</li>
+                </ul>
+                <p>Cada mes se añaden aproximadamente 25.000 nuevos contenidos a IEEE Xplore.</p>
                 <?php
-                    $doc = new DOMDocument();
-                    libxml_use_internal_errors(true);
                     if(!empty($_POST["buscar"])) {
                         $url=$ParserIEEEXplore->getUrlBusqueda();
-                        //$url="https://ieeexplore.ieee.org/search/searchresult.jsp?newsearch=true&queryText=".str_replace(' ', '%20', $_POST["buscar"]);
                     }
                     else {
                         $url="https://ieeexplore.ieee.org/Xplore/home.jsp";
                     }
-                    /*$doc->loadHTMLFile($url);
-                    echo $doc->saveHTML();*/
                 ?>
-                <a <?php echo "href='".$url."'"; ?> target="_blank">Enlace a IEEE Xplore</a>
+                <a <?php echo "href='".$url."'"; ?> target="_blank"><button type="button" class="btn btn-primary">Enlace a IEEE Xplore</button></a>
             </div>
             <div class="tab-pane fade" id="nav-springerlink" role="tabpanel" aria-labelledby="nav-springerlink-tab">
+                <p>Como parte de Springer Nature, SpringerLink ofrece acceso rápido a la profundidad y amplitud de su colección en línea de revistas, libros electrónicos, obras de referencia y protocolos en una amplia gama de disciplinas.</p>
+                <p>SpringerLink es una plataforma de lectura utilizada por cientos de miles de investigadores de todo el mundo.</p>
+                <p>Permite la búsqueda de artículos de investigación, libros académicos y más.</p>
+                <ul>
+                    <li>200 millones de descargas mensuales</li>
+                    <li>24 millones de lectores mensuales</li>
+                    <li>3 millones de autores suben contenido anualmente</li>
+                </ul>
                 <?php
-                    $doc = new DOMDocument();
-                    libxml_use_internal_errors(true);
                     if(!empty($_POST["buscar"])) {
                         $url=$ParserSpringerLink->getUrlBusqueda();
-                        //$url="https://link.springer.com/search?new-search=true&query=".str_replace(' ', '+', $_POST["buscar"]);
                     }
                     else {
                         $url="https://link.springer.com";
                     }
-                    /*$doc->loadHTMLFile($url);
-                    echo $doc->saveHTML();*/
                 ?>
-                <a <?php echo "href='".$url."'"; ?> target="_blank">Enlace a SpringerLink</a>
+                <a <?php echo "href='".$url."'"; ?> target="_blank"><button type="button" class="btn btn-primary">Enlace a SpringerLink</button></a>
             </div>
         </div>
+        <footer>
+            <p><a href="disclaimers.php">Disclaimers</a></p>
+        </footer>
     </body>
 </html>
---
+++ disclaimers.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Disclaimers</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <h1 style="text-align:center;">Disclaimers</h1>
+        <p></p>
+        <h3>Cláusula de limitación de responsabilidad</h3>
+        <p>No se responsabilizará a los desarrolladores de la aplicación por:</p>
+        <ul>
+            <li>Daños de cualquier tipo que puedan ser causados por el uso o incapacidad de utilizar correctamente la aplicación.</li>
+            <li>Errores o fallos para mostrar la información.</li>
+            <li>Daño personal o daño a la propiedad que pueda resultar del uso de la aplicación.</li>
+            <li>Interrupción o cese de la transmisión desde o hacia el servicio.</li>
+        </ul>
+        <p></p>
+        <h3>Cláusula de exclusión de garantía</h3>
+        <p>La información contenida en la aplicación se visualiza tal cual es prevista por las bases de datos utilizadas.</p>
+        <p>La búsqueda y los resultados que se muestren dependen enteramente de los proveedores de las bases de datos.</p>
+        <p>Esta aplicación simplemente facilita la realización de una búsqueda en dichos sitios, y su correcto funcionamiento dependerá del funcionamiento de dichos sitios.</p>
+        <p></p>
+        <h3>Cláusula de exclusión sobre el contenido</h3>
+        <p>El contenido de la aplicación tiene finalidades informativas.</p>
+        <p>Dicho contenido contenido es provisto por los proveedores de las bases de datos.</p>
+        <p>Los resultados mostrados no dependen de la aplicación.</p>
+    </body>
+</html>
