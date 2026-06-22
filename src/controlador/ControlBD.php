6da9d0ee8b367f94889ef7ae8c926411a1203712
UrielUriel <paredesuri@gmail.com>
2026-06-22 16:32:25 -0300

    [22/06/2026] Manejo de excepciones para las operaciones sobre la base de datos

--- src/controlador/ControlBD.php
+++ src/controlador/ControlBD.php
@@ -3,6 +3,7 @@
 namespace Controlador;
 
 use Mysqli;
+use mysqli_sql_exception;
 use Modelo\Busqueda;
 
 class ControlBD
@@ -15,13 +16,21 @@ class ControlBD
         $this->arrayPost = $arrayPost;
     }
 
+    /**
+     * @return void
+     * @throws mysqli_sql_exception En el caso de que ocurra un error al conectarse con la base de datos
+     */
     protected function abrirConexion()
     {
-        $host = "localhost";
-        $usuario = "root";
-        $contraseña = "";
-        $bd = "bdpaperseek";
-        $this->conexion = new mysqli($host, $usuario, $contraseña, $bd) or die("Conexión falló: %s\n" . $this->conexion -> error);
+        try {
+            $host = "localhost";
+            $usuario = "root";
+            $contraseña = "";
+            $bd = "bdpaperseek";
+            $this->conexion = new mysqli($host, $usuario, $contraseña, $bd);
+        } catch (mysqli_sql_exception $e) {
+            error_log("Error al conectar con la base de datos: " . $e->getMessage());
+        }
     }
 
     protected function cerrarConexion()
@@ -38,23 +47,26 @@ class ControlBD
      * @param  Busqueda $Busqueda Datos de busqueda recibidos en el formulario
      * 
      * @return void
+     * @throws mysqli_sql_exception En el caso de que no permita registrar la búsqueda en la base de datos
      */
     public function crearBusqueda($Busqueda)
     {
-        $cadenaBusqueda = $Busqueda->getCadenaBusqueda();
-        $checkACM = $Busqueda->getCheckACM();
-        $checkScienceDirect = $Busqueda->getCheckScienceDirect();
-        $checkIEEEXplore = $Busqueda->getCheckIEEEXplore();
-        $checkSpringerLink = $Busqueda->getCheckSpringerLink();
-        $añoInicio = $Busqueda->getAñoInicio();
-        $añoFin = $Busqueda->getAñoFin();
-        $this->abrirConexion();
-        $sql = $this->conexion->prepare("INSERT INTO busqueda(cadena_busqueda, check_acm, check_sciencedirect, check_ieee_xplore, check_springerlink, año_inicio, año_fin) VALUES (?, ?, ?, ?, ?, ?, ?)");
-        $sql->bind_param("siiiiii", $cadenaBusqueda, $checkACM, $checkScienceDirect, $checkIEEEXplore, $checkSpringerLink, $añoInicio, $añoFin);
-        $resultado = $sql->execute();
-        if (!$resultado) {
-            echo "Error al registrar la búsqueda";
+        try {
+            $cadenaBusqueda = $Busqueda->getCadenaBusqueda();
+            $checkACM = $Busqueda->getCheckACM();
+            $checkScienceDirect = $Busqueda->getCheckScienceDirect();
+            $checkIEEEXplore = $Busqueda->getCheckIEEEXplore();
+            $checkSpringerLink = $Busqueda->getCheckSpringerLink();
+            $añoInicio = $Busqueda->getAñoInicio();
+            $añoFin = $Busqueda->getAñoFin();
+            $this->abrirConexion();
+            $sql = $this->conexion->prepare("INSERT INTO busqueda(cadena_busqueda, check_acm, check_sciencedirect, check_ieee_xplore, check_springerlink, año_inicio, año_fin) VALUES (?, ?, ?, ?, ?, ?, ?)");
+            $sql->bind_param("siiiiii", $cadenaBusqueda, $checkACM, $checkScienceDirect, $checkIEEEXplore, $checkSpringerLink, $añoInicio, $añoFin);
+            $sql->execute();
+        } catch (mysqli_sql_exception $e) {
+            error_log("Error al registrar la búsqueda: " . $e->getMessage());
+        } finally {
+            $this->cerrarConexion();
         }
-        $this->cerrarConexion();
     }
 }
